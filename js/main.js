3deeed69ecb36af642088dcd09bcb4890fdb41eb
Manoj Satishkumar <33158401+ManojSatishkumar@users.noreply.github.com>
2019-02-19 20:56:00 +0530

    refined more

--- js/main.js
+++ js/main.js
@@ -1,45 +1,49 @@
 /*global $, jQuery, alert*/
 $(document).ready(function() {
-
-  'use strict';
+  "use strict";
 
   // ========================================================================= //
   //  //SMOOTH SCROLL
   // ========================================================================= //
 
-
   $(document).on("scroll", onScroll);
 
-  $('a[href^="#"]').on('click', function(e) {
+  $('a[href^="#"]').on("click", function(e) {
     e.preventDefault();
     $(document).off("scroll");
 
-    $('a').each(function() {
-      $(this).removeClass('active');
+    $("a").each(function() {
+      $(this).removeClass("active");
       if ($(window).width() < 768) {
-        $('.nav-menu').slideUp();
+        $(".nav-menu").slideUp();
       }
     });
 
-    $(this).addClass('active');
+    $(this).addClass("active");
 
     var target = this.hash,
-        menu = target;
+      menu = target;
 
     target = $(target);
-    $('html, body').stop().animate({
-      'scrollTop': target.offset().top - 80
-    }, 500, 'swing', function() {
-      window.location.hash = target.selector;
-      $(document).on("scroll", onScroll);
-    });
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: target.offset().top - 80
+        },
+        500,
+        "swing",
+        function() {
+          window.location.hash = target.selector;
+          $(document).on("scroll", onScroll);
+        }
+      );
   });
 
-
   function onScroll(event) {
-    if ($('.home').length) {
+    if ($(".home").length) {
       var scrollPos = $(document).scrollTop();
-      $('nav ul li a').each(function() {
+      $("nav ul li a").each(function() {
         var currLink = $(this);
         var refElement = $(currLink.attr("href"));
       });
@@ -50,16 +54,15 @@ $(document).ready(function() {
   //  //NAVBAR SHOW - HIDE
   // ========================================================================= //
 
-
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
-    if (scroll > 200 ) {
+    if (scroll > 200) {
       $("#main-nav, #main-nav-subpage").slideDown(700);
-      $("#main-nav-subpage").removeClass('subpage-nav');
+      $("#main-nav-subpage").removeClass("subpage-nav");
     } else {
       $("#main-nav").slideUp(700);
       $("#main-nav-subpage").hide();
-      $("#main-nav-subpage").addClass('subpage-nav');
+      $("#main-nav-subpage").addClass("subpage-nav");
     }
   });
 
@@ -67,8 +70,8 @@ $(document).ready(function() {
   //  // RESPONSIVE MENU
   // ========================================================================= //
 
-  $('.responsive').on('click', function(e) {
-    $('.nav-menu').slideToggle();
+  $(".responsive").on("click", function(e) {
+    $(".nav-menu").slideToggle();
   });
 
   // ========================================================================= //
@@ -79,56 +82,51 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Manoj.", " a Developer.", " an Artist.", "an Architect.", "a Traveller"],
+      strings: ["Manoj.", " a Developer.", " a Blogger.", "a Traveller"],
       typeSpeed: 100,
-      loop: true,
+      loop: true
     });
   });
-
 
   // ========================================================================= //
   //  Owl Carousel Services
   // ========================================================================= //
 
-
-  $('.services-carousel').owlCarousel({
-      autoplay: true,
-      loop: true,
-      margin: 20,
-      dots: true,
-      nav: false,
-      responsiveClass: true,
-      responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 4 } }
-    });
-
+  $(".services-carousel").owlCarousel({
+    autoplay: true,
+    loop: true,
+    margin: 20,
+    dots: true,
+    nav: false,
+    responsiveClass: true,
+    responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 4 } }
+  });
 
   // ========================================================================= //
   //  Porfolio isotope and filter
   // ========================================================================= //
 
-
-  var portfolioIsotope = $('.portfolio-container').isotope({
-    itemSelector: '.portfolio-thumbnail',
-    layoutMode: 'fitRows'
+  var portfolioIsotope = $(".portfolio-container").isotope({
+    itemSelector: ".portfolio-thumbnail",
+    layoutMode: "fitRows"
   });
 
-  $('#portfolio-flters li').on( 'click', function() {
-    $("#portfolio-flters li").removeClass('filter-active');
-    $(this).addClass('filter-active');
+  $("#portfolio-flters li").on("click", function() {
+    $("#portfolio-flters li").removeClass("filter-active");
+    $(this).addClass("filter-active");
 
-    portfolioIsotope.isotope({ filter: $(this).data('filter') });
+    portfolioIsotope.isotope({ filter: $(this).data("filter") });
   });
-
 
   // ========================================================================= //
   //  magnificPopup
   // ========================================================================= //
 
   var magnifPopup = function() {
-    $('.popup-img').magnificPopup({
-      type: 'image',
+    $(".popup-img").magnificPopup({
+      type: "image",
       removalDelay: 300,
-      mainClass: 'mfp-with-zoom',
+      mainClass: "mfp-with-zoom",
       gallery: {
         enabled: true
       },
@@ -136,7 +134,7 @@ $(document).ready(function() {
         enabled: true, // By default it's false, so don't forget to enable it
 
         duration: 300, // duration of the effect, in milliseconds
-        easing: 'ease-in-out', // CSS transition easing function
+        easing: "ease-in-out", // CSS transition easing function
 
         // The "opener" function should return the element from which popup will be zoomed in
         // and to which popup will be scaled down
@@ -144,14 +142,14 @@ $(document).ready(function() {
         opener: function(openerElement) {
           // openerElement is the element on which popup was initialized, in this case its <a> tag
           // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-          return openerElement.is('img') ? openerElement : openerElement.find('img');
+          return openerElement.is("img")
+            ? openerElement
+            : openerElement.find("img");
         }
       }
     });
   };
 
-
   // Call the functions
   magnifPopup();
-
 });
