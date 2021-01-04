8b02c9526f4499193d48537c5272298c297705ae
Manoj Satishkumar <manoj.satishkumar@walmartlabs.com>
2021-01-05 00:48:21 +0530

    auto push commit

--- js/main.js
+++ js/main.js
@@ -114,5 +114,6 @@ $(document).ready(function () {
     $("#desktop-nav-bar").load("/components/desktop-nav-bar.html");
     $("#mobile-nav-bar").load("/components/mobile-nav-bar.html");
     $("#footer").load("/components/footer.html");
+    $("#article-suggestions").load("/blog/index.html");
   });
 });
