9a682e64848dbb3dfe5619ad7c10cbfa5f5982f6
Manoj Satishkumar <manoj.satishkumar@walmartlabs.com>
2021-01-10 19:32:27 +0530

    auto push commit

--- js/main.js
+++ js/main.js
@@ -116,3 +116,18 @@ $(document).ready(function () {
     $("#footer").load("/components/footer.html");
   });
 });
+
+window.addEventListener('load', () => {
+  registerSW();
+});
+
+
+async function registerSW() {
+  if ('serviceWorker' in navigator) {
+    try {
+      await navigator.serviceWorker.register('./sw.js');
+    } catch (e) {
+      console.log(`SW registration failed`);
+    }
+  }
+}
---
+++ sw.js
@@ -0,0 +1,43 @@
+const cacheName = 'msk-web-development';
+const staticAssets = [
+  './index.html',
+];
+
+self.addEventListener('install', async e => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', async e => {
+  const req = e.request;
+  const url = new URL(req.url);
+
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
+  }
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const fresh = await fetch(req);
+    await cache.put(req, fresh.clone());
+    return fresh;
+  } catch (e) {
+    const cached = await cache.match(req);
+    return cached;
+  }
+}
