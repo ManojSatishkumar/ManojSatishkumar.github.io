1de24566ae82a2efc12d6e559869b7da08fe3554
Manoj Satishkumar <manoj.satishkumar@walmartlabs.com>
2021-01-11 01:03:06 +0530

    auto push commit

--- js/main.js
+++ js/main.js
@@ -144,7 +144,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
     
   }
   // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
+  // e.preventDefault();
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
   // Update UI notify the user they can install the PWA
