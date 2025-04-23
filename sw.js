b71465f0413998868285d6e6e64fa7f3f7ebd95a
Manoj Satishkumar <85manojs@gmail.com>
2025-04-23 20:40:46 +0530

    Added PWA support

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "my-pwa-cache-v1";
+const urlsToCache = ["/", "/index.html", "/css/styles.css", "/js/main.js"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
